c877567041f608f099639826c50bc283aedafc0d
Max Plomer <maxplomer@gmail.com>
2016-07-01 19:09:15 -0400

    add input for newNodeValue

--- app.jsx
+++ app.jsx
@@ -1,12 +1,15 @@
 var Tree = React.createClass({
   getInitialState: function() {
-    var myState= { tree: [] };
+    var myState= {
+      tree: [1,2,3,4],
+      newNodeValue: 1234
+    };
 
     return myState;
   },
 
   handleChange: function(event) {
-
+    this.setState({newNodeValue: event.target.value});
   },
 
   componentDidUpdate: function() {
@@ -16,7 +19,10 @@ var Tree = React.createClass({
   render: function() {
     return (
       <div>
-        HELLOWORLD
+        <input type="number" min="0" step="1" value={this.state.newNodeValue} onChange={this.handleChange} placeholder="Enter integer"/>
+        <button>Add to tree</button>
+        <br/>
+        { this.state.tree }
       </div>
     );
   }
